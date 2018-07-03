1d76143b4348dc9d8a316f870acd22f63b9613ae
bbabou94@gmail.com <bbabou94@gmail.com>
2018-07-03 02:39:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function whisper(string)  {
+  return whisper.toLowerCase()
+}
+
+function shout(string) {
+  return shout.toUpperCase()
+}
+
+funtion logShout(string) {
+  console.log(logShout.toUpperCase())
+}
